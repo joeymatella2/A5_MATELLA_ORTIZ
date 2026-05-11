75bc268ea49e1703036bd3f26e78d636c1a305ac
joeymatella2 <111457339+joeymatella2@users.noreply.github.com>
2026-05-11 16:17:24 -0700

    Create dac.h for DAC initialization and writing
    Add header file for DAC functionality with initialization and write functions.

---
+++ dac.h
@@ -0,0 +1,42 @@
+/*
+ *******************************************************************************
+ * @file           : dac.h
+ * @brief          : X
+ * project         : EE 329 S'26 AX
+ * authors         : joeym
+ * version         : 0.1
+ * date            : May 11, 2026
+ * compiler        : STM32CubeIDE v.1.19.0 Build: 14980_20230301_1550 (UTC)
+ * target          : NUCLEO-L4A6ZG
+ * clocks          : 4 MHz MSI to AHB2
+ * @attention      : (c) 2026 STMicroelectronics.  All rights reserved.
+ *******************************************************************************
+ * Description: X
+ *
+ *******************************************************************************
+ * GPIO Wiring
+ * |   Component    | GPIO Identifier | Connector Location | Config
+ *-----------------------------------------------------------------------------
+ * | LCD - DB4 - 11 | PC0             | CN9-3              | OUT
+ *******************************************************************************
+ * Version History
+ *  Ver.|   Date   |  Description
+ *  ---------------------------------------------------------------------------
+ *      |          | 
+ *******************************************************************************
+ *
+ * Header format adapted from [Code Appendix by Kevin Vo] pg 5
+ */
+
+
+#ifndef INC_DAC_H_
+#define INC_DAC_H_
+
+#include "stm32l4xx_hal.h"
+
+void DAC_init(void);
+void DAC_write(uint16_t data);
+
+
+
+#endif /* INC_DAC_H_ */
